5f2a2074a7919c123818d1647271bfa48a3adbd2
Mikhail Medvedev <e-ink-reader@yandex.ru>
2024-12-09 15:57:29 +0300

    Translations added

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,6 @@
 rm -rf /usr/bin/SFP-Master
 rm -rf /usr/share/pixmaps/write64.png
+rm -rf /usr/share/applications/SFP-Master.desktop
+rm -rf /usr/share/sfp-master
+rm -rf /usr/share/man/man1/SFP-Master.1.gz
+rm -rf /usr/share/metainfo/io.github.bigbigmdm.sfp-master.metainfo.xml
