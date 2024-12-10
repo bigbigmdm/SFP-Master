9709ba08bb1fb29342bd40364593813ef1358136
Mikhail Medvedev <e-ink-reader@yandex.ru>
2024-12-10 09:07:47 +0300

    pdf help added

--- uninstall.sh
+++ uninstall.sh
@@ -4,3 +4,4 @@ rm -rf /usr/share/applications/SFP-Master.desktop
 rm -rf /usr/share/sfp-master
 rm -rf /usr/share/man/man1/SFP-Master.1.gz
 rm -rf /usr/share/metainfo/io.github.bigbigmdm.sfp-master.metainfo.xml
+rm -rf /usr/share/doc/sfp-master
